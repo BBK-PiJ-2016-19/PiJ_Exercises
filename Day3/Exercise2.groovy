a7c900700c82af5233bd43c7ab3a2b58bee5d9db
Miss Sonia Da Trindade Brito Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-02 15:37:19 +0000

    Day3 files added

--- Day3/Exercise2.groovy
+++
@@ -1,6 +0,0 @@
-// Set of exercises for Lecture 3
-
-//Exercise 2 - Calculator
-
-String str = System.console().readline()
-
---
+++ Day3/Q2.groovy
@@ -0,0 +1,22 @@
+// Set of exercises for Lecture 3
+
+//Exercise 2 - Calculator
+
+println ""
+println "This program reads two numbers and perform the four basic operations."
+println ""
+println "Insert two numbers:"
+
+String str
+str = System.console().readline()
+println str
+
+/*//String str1 = System.console().readline()
+//String str2 = System.console().readLine()
+
+//int num1 = Integer.parseInt(str1)
+//int num2 = Integer.parseInt(str2)
+
+//println "The first number is: " + num1
+//println "The second number is: " + num2*/
+
