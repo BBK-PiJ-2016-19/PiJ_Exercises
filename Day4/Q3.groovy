d6bd798328e483142cc13d8a1644235bd07ccf1c
Sonia Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-08 23:37:04 +0000

    Implementation of the binaryToDecimal method

--- Day4/Q3.groovy
+++ Day4/Q3.groovy
@@ -2,7 +2,7 @@
 
 //Q3 - Binary and decimal
 
-//1st method: power(int, int)
+//1st method: power(int, int) // Fazer à mão!
 int power(int base, int exponent) { 
   int result;
   result = base ** exponent;
@@ -15,3 +15,24 @@ int power2(int exponent) {
 	result = power(2, exponent);
 	return result;
 }
+
+//3rd method: binaryToDecimal(in)
+int binaryToDecimal (String number) {
+
+	int result = 0;
+	
+	for (i = 0; i < number.length(); i++) {
+		result += Integer.parseInt(number[i]) * power2(number.length()-(i+1));
+	}
+	
+	return result;
+}
+
+
+//Testing the binaryToDecimal()
+int result;
+String binaryNumber1 = "10000001"
+result = binaryToDecimal(binaryNumber1)
+println ""
+println "Binary number: " + binaryNumber1
+println "The correspondent decimal: " + result
