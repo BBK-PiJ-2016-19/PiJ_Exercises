0bac2f2edd05f630b12db715508e427ae45fb886
Sonia Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-10 13:13:02 +0000

    Addition of the script

--- Day4/Q6.groovy
+++ Day4/Q6.groovy
@@ -31,3 +31,28 @@ class Integer2 {
 	}
 }
 
+
+// Script
+Integer2 i2 = new Integer2();
+
+println ""
+print "Enter a number: ";
+String str = System.console().readLine();
+
+int i = Integer.parseInt(str);
+i2.setValue(i);
+
+print "The number you entered is "
+if (i2.isEven()) {
+	println "even.";
+} else if (i2.isOdd()) {
+	println "test"
+	println "odd.";
+} else {
+	println "undefined! Your code is buggy!";
+}
+
+//int parsedInt = Integer.parseInt(i2.toString());
+//if (parseInt == i2.getValue()) {
+//	println("Your toString() method seems to work fine.");
+//}
