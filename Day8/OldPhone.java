042e67d093b9c78fe83257707b9fa961176f7206
Miss Sonia Da Trindade Brito Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-21 21:03:12 +0000

    Cleaned up

--- Day8/OldPhone.java
+++ Day8/OldPhone.java
@@ -1,11 +1,5 @@
 public class OldPhone implements Phone {
 
-	/* private String number;
-
-	public OldPhone(String number) {
-		this.number = number;
-	} */
-
 	public void call(String number) {
 		System.out.println("Calling " + number + "...");
 	}
