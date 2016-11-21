18ce98dddb7810619902b42e1bc0f17e64d1d13e
Miss Sonia Da Trindade Brito Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-21 21:04:23 +0000

    Added new calls

--- Day8/PhoneLauncher.java
+++ Day8/PhoneLauncher.java
@@ -8,6 +8,14 @@ public class PhoneLauncher {
 	public void launch() {
 		// code creating and using SmartPhone here...
 		SmartPhone smtPhone = new SmartPhone();
+		
 		smtPhone.call("075234567");
+		
+		smtPhone.browseWeb("http://www.bbc.co.uk/");
+		String position = smtPhone.findPosition();
+		System.out.println(position);
+		
+		smtPhone.sayGreeting("Good morning");
+		
 	}
 }
