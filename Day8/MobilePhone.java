ae7bcf8c06b2dec76c94c66dc3bfd0ceb1003322
Miss Sonia Da Trindade Brito Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-21 21:02:19 +0000

    Bug fixed

--- Day8/MobilePhone.java
+++ Day8/MobilePhone.java
@@ -1,20 +1,21 @@
 public class MobilePhone extends OldPhone {
 	
-	private String[] calledNumbers;
+	private String[] calledNumbers; //keeps a list of the last ten numbers
+	
 	
 	public void sayGreeting(String greeting) {
 		System.out.println(greeting);
 	}
 	
 	public void ringAlarm(String alarm) {
-		System.out.println(sound);
+		System.out.println(alarm);
 	}
 	
 	public void playGame(String game) {
 		System.out.println(game);
 	}
 	
-	public void printLast10Numbers() {
+	public void printLastNumbers() {
 		// need to be implemented
 	}
 }
