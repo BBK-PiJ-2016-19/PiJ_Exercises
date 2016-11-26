5a09c2b6926a46832f49d3e07e66659ad2268daa
Sonia Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-26 14:21:48 +0000

    Implemented class Patient

---
+++ Day6/Patient.java
@@ -0,0 +1,40 @@
+public class Patient {
+	
+	//fields
+	private String name;
+	private int age;
+	private String illness;
+	private Patient nextPatient;
+	
+	
+	//constructor
+	public Patient(String name, int age, String illness) {
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.nextPatient = null; // Pointer to another patient (to come!)
+	}
+	
+	
+	//other methods - getters and setters
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getIllness() {
+		return illness;
+	}
+	
+	public Patient getNextPatient() {
+		return nextPatient;
+	}
+	
+	public void setNextPatient(Patient nextPatient) {
+		this.nextPatient = nextPatient;
+	}
+	
+}
