eadaa70d50d28f6ae031de0b2aa9129e800a108e
Sonia Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-26 13:32:03 +0000

    Commented the inputs print lines

--- Day3/Q2.java
+++ Day3/Q2.java
@@ -10,21 +10,21 @@ public class Q2 {
 		
 		Calculator calculator = new Calculator();
 		
-		System.out.println("Insert the operator: ");
+		//System.out.println("Insert the operator: ");
 		//String str = ...
 		//calculator.setOperator(str);
 		calculator.setOperator("+");
 		
-		System.out.println("Insert the first number: ");
+		//System.out.println("Insert the first number: ");
 		String str1 = "6";
 		calculator.setFirstNumber(Double.parseDouble(str1));
 		
-		System.out.println("Insert the second number: ");
+		//System.out.println("Insert the second number: ");
 		String str2 = "3";
 		//calculator.setSecondNumber() = Double.parseDouble(str2); Duh!
 		calculator.setSecondNumber(Double.parseDouble(str2));
 		
-		System.out.println("");
+		//System.out.println("");
 		System.out.println("The result is: " + calculator.operate());
 	}
 
