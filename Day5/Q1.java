9701761fae73a8d9c2b879834da127ad2181f258
Sonia Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-26 16:48:45 +0000

    Revert "Experiment: using script in groovy to call java classes"
    This reverts commit d4299a32103dcea0826497f521ab83b4133ab7bb.

--- Day5/Q1.java
+++ Day5/Q1.java
@@ -2,19 +2,17 @@ public class Q1 {
 	
 	public static void main(String[] args) {
 		
-		Calculator myCalc = new Calculator(0, 0);
+		Calculator myCalc = new Calculator();
 	
 		//int i = 10;
 		
-		/*
 		System.out.println("");
 		System.out.println("Calculator");
 		myCalc.add(10, 12);
 		myCalc.subtract(10, 8);
 		myCalc.multiply(10, 10);
 		myCalc.divide(10, 10);
-		*/
-		
+	
 	} 
 	
 }
