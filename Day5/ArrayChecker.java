88bf122742d762f2d1aecad1ca7555b405eccea4
Sonia Goncalves <sbrito01@dcs.bbk.ac.uk>
2016-11-26 14:13:50 +0000

    Implemented the isSymmetrical and reverse methods

--- Day5/ArrayChecker.java
+++ Day5/ArrayChecker.java
@@ -2,10 +2,21 @@ public class ArrayChecker {
 	
 	
 	public boolean isSymmetrical(int[] arrayInts) {
-		return false;
+		int size = arrayInts.length();
+		for(int i = 0; i < size/2; i++) {
+			if(arrayInts[i] != arrayInts[size-i-1]) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	public int[] reverse(int[] arrayInts) {
-		return = null;
+		int size = arrayInts.length();
+		int[] result = new int[size];
+		for (int i = 0; i < ize; i++) {
+			result[size-1-i] = arrayInts[i];
+		}
+		return result;
 	}
 }
